b3c57f06a811df3d41aff2af320239f482c18dc2
Jinhan <wlsgks333@naver.com>
2024-11-11 01:29:39 +0900

    Nav 컴포넌트 css 파일 작성.

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,8 @@ import mainLogo from "../../assets/images/logo/MainLogoMiddle.png";
 
 const getLinkStyle = ({ isActive }) => {
   return {
-    color: isActive ? "##FFFFFF" : undefined,
+    color: isActive ? "##FFFFFF" : "#747474",
+    
   };
 };
 
@@ -18,18 +19,26 @@ function Nav() {
           </Link>
         </div>
         <div className="navMenu">
-          <NavLink to="/all-startup-list" style={getLinkStyle}>
-            전체 기업
-          </NavLink>
-          <NavLink to="/compare" style={getLinkStyle}>
-            나의 기업 비교
-          </NavLink>
-          <NavLink to="/compare-status" style={getLinkStyle}>
-            비교 현황
-          </NavLink>
-          <NavLink to="/invest-status" style={getLinkStyle}>
-            투자 현황
-          </NavLink>
+          <div className="navMenuItem">
+            <NavLink to="/all-startup-list" style={getLinkStyle}>
+              전체 기업
+            </NavLink>
+          </div>
+          <div className="navMenuItem">
+            <NavLink to="/compare" style={getLinkStyle}>
+              나의 기업 비교
+            </NavLink>
+          </div>
+          <div className="navMenuItem">
+            <NavLink to="/compare-status" style={getLinkStyle}>
+              비교 현황
+            </NavLink>
+          </div>
+          <div className="navMenuItem">
+            <NavLink to="/invest-status" style={getLinkStyle}>
+              투자 현황
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
