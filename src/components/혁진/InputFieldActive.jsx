13662842f653b8391f2600cb7a67cc5c43aef116
김혁진 <kimhyuckjin7856@gmail.com>
2024-11-11 03:25:02 +0900

    Add input field components

---
+++ src/components/혁진/InputFieldActive.jsx
@@ -0,0 +1,7 @@
+import "../혁진/css파일/input.css";
+
+function InputActive() {
+  return <div className="input"></div>;
+}
+
+export default InputActive;
---
+++ src/components/혁진/InputFieldInactive.jsx
@@ -0,0 +1,11 @@
+import "../혁진/css파일/input.css";
+
+function InputInactive() {
+  return (
+    <div className="Inactive">
+      <div className="InactiveFill"></div>
+    </div>
+  );
+}
+
+export default InputInactive;
