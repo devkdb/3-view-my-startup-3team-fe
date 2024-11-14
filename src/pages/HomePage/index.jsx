b6eb52798ec2c8cf8f46952ef2a5aaecc5b5da28
Jinhan <wlsgks333@naver.com>
2024-11-15 02:08:16 +0900

    여러 부분에서 CSS추가 및 깃허브 아이콘 추가 링크 연결

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import './index.css';
-import homeLogoImage from '../../assets/images/home/MainLogoBig.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+import homeLogoImage from "../../assets/images/home/MainLogoBig.png";
 
-function HomePage(){
+function HomePage() {
   return (
     <div id="homePage">
       <img className="homeMainLogo" src={homeLogoImage} alt="홈 메인 로고" />
-      HomePage
+      <div className="mainTextBox">
+        <div className="mainText1">스타트업을 검색하고</div>
+        <div className="mainText2">모의 투자를 시작하세요!</div>
+      </div>
+      <div className="mainButtionContainer">
+        <button className="mainButtion">
+          <Link to="/all-startup-list">투자 시작하기</Link>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage;
