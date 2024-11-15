ace3bc8cc2d319c692e64304679ba5f57a9622dd
Jinhan <wlsgks333@naver.com>
2024-11-15 14:08:39 +0900

    AllStartupListPage, startupList 부분 구현

--- src/pages/AllStartupListPage/index.jsx
+++ src/pages/AllStartupListPage/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 import SearchComponent from "../../components/Search/index";
 import AllStartupDropdown from "./components/Dropdown/index";
 import StartupList from "./components/StartupList/index";
-import PageList from "../../components/PageList/index.jsx";
+import PageList from "./components/PageList/index.jsx";
 
 function AllStartupListPage() {
+  const ten = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
   return (
     <div id="allStartupListPage">
       <div className="title">
@@ -16,9 +19,13 @@ function AllStartupListPage() {
         </div>
       </div>
       <StartupList />
-      <PageList />
+      <div className="pageList">
+      {ten.map((item, index) => {
+        return <Link key={index} to="Details/companyId"><PageList /></Link>
+      }
+        )}
+        </div>
     </div>
-    
   );
 }
 
