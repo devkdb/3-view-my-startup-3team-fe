8d5777963b96a738ff69d01bb30ca307c3e5af68
Bluemoon105 <118960498+Bluemoon105@users.noreply.github.com>
2024-11-25 14:50:32 +0900

    투자 현황 페이지 작업

--- src/pages/InvestStatusPage/components/PageList/index.jsx
+++ src/pages/InvestStatusPage/components/PageList/index.jsx
@@ -1,31 +1,32 @@
 import "./index.css";
-import codeItImg from "../../../../assets/images/logo/codeItImg.png";
 
-function PageList() {
+function PageList({startup, index}) {
+
+  const changeActualInvest = Math.floor(startup.actualInvest / 100000000);
+  const changeSimInvest = Math.floor(startup.simInvest / 100000000);
+
   return (
       <div className="pageListItem">
         <div className="pageListItemLank pageListItemPosition basicText">
-          1위
+          {index}위
         </div>
         <div className="pageListItemName pageListItemPosition">
-          <img src={codeItImg} alt="스타트업 이미지" />
-          <span>코드잇</span>
+          <img src={startup.image} alt="스타트업 이미지" />
+          <span>{startup.name}</span>
         </div>
         <div className="pageListItemDescription pageListItemPosition">
           <span className="basicText">
-            코드잇은 '온라인 코딩 교육 서비스'를 운영하는 EdTech 스타트업입니다.
-            코딩 교육에 대한 빠른 시작, 쉽고 편리한 지원을 제공하며 실무에서
-            개발하는 방법을 학습할 수 있는 플랫폼입니다.
+           {startup.description}
           </span>
         </div>
         <div className="pageListItemCategory pageListItemPosition basicText">
-          에듀테크
+          {startup.Category.category}
         </div>
         <div className="investmentAmount pageListItemPosition basicText">
-          100억원
+          {changeSimInvest}억원
         </div>
         <div className="pageListItemRevenueTotal pageListItemPosition basicText">
-          50억원
+          {changeActualInvest}억원
         </div>
       </div>
   );
