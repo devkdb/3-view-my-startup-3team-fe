11bb68ff6cdc08e4754f3842ce9f7ac1225a89d0
jonghun4 <xwhdgns@gmail.com>
2024-11-25 11:46:03 +0900

    페이지 작업 완료

--- src/pages/CompareStatusPage/components/Dropdown/List/index.jsx
+++ src/pages/CompareStatusPage/components/Dropdown/List/index.jsx
@@ -1,9 +1,10 @@
+import React from "react";
 import "../index.css";
 
 function CompareStatusDropdownList({ onItemClick, dropdownRef }) {
-  // 리스트 항목들을 클릭하면 onItemClick 함수가 호출되어 선택된 값을 전달
   const dropdownRect = dropdownRef.current?.getBoundingClientRect();
   const listPosition = dropdownRect ? dropdownRect.bottom + window.scrollY : 0; // 드롭다운 박스 바로 아래에 리스트 위치
+
   return (
     <ul
       className="DropdownListLayerCompare"
@@ -13,25 +14,33 @@ function CompareStatusDropdownList({ onItemClick, dropdownRef }) {
     >
       <li
         className="DropdownListFontCompare"
-        onClick={() => onItemClick("나의 기업 선택 횟수 높은순")}
+        onClick={() =>
+          onItemClick("selectCountDesc", "나의 기업 선택 횟수 높은순")
+        }
       >
         나의 기업 선택 횟수 높은순
       </li>
       <li
         className="DropdownListFontCompare"
-        onClick={() => onItemClick("나의 기업 선택 횟수 낮은순")}
+        onClick={() =>
+          onItemClick("selectCountAsc", "나의 기업 선택 횟수 낮은순")
+        }
       >
         나의 기업 선택 횟수 낮은순
       </li>
       <li
         className="DropdownListFontCompare"
-        onClick={() => onItemClick("실제 누적 투자 금액 높은순")}
+        onClick={() =>
+          onItemClick("actualInvestDesc", "실제 누적 투자 금액 높은순")
+        }
       >
         실제 누적 투자 금액 높은순
       </li>
       <li
         className="DropdownListFontCompare"
-        onClick={() => onItemClick("실제 누적 투자 금액 낮은순")}
+        onClick={() =>
+          onItemClick("actualInvestAsc", "실제 누적 투자 금액 낮은순")
+        }
       >
         실제 누적 투자 금액 낮은순
       </li>
