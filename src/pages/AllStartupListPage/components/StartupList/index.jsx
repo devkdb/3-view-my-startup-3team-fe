d1edbd6d0b3240af7407529022a5b2909a75e36e
Jinhan <wlsgks333@naver.com>
2024-11-26 14:25:07 +0900

    css 수정중

--- src/pages/AllStartupListPage/components/StartupList/index.jsx
+++ src/pages/AllStartupListPage/components/StartupList/index.jsx
@@ -8,12 +8,12 @@ function StartupList({startups}) {
         <tbody>
           <tr>
             <th className="startupRank hide">순위</th>
-            <th className="startupName">기업 명</th>
-            <th className="startupIntro">기업 소개</th>
+            <th className="allStartupName">기업 명</th>
+            <th className="allStartupIntro">기업 소개</th>
             <th className="allStartupCategory">카테고리</th>
-            <th className="startupInvest">누적 투자 금액</th>
-            <th className="startupRevenue">매출액</th>
-            <th className="startupEmployee">고용인원</th>
+            <th className="allStartupInvest">누적 투자 금액</th>
+            <th className="allStartupRevenue">매출액</th>
+            <th className="allStartupEmployee">고용인원</th>
           </tr>
         </tbody>
       </table>
